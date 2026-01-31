48a1fe8020bf6d878d03914c2aac1142e3d41c82
Krzysztof Wojnar <krzysztofwojnar@outlook.com>
2026-01-31 21:14:10 +0100

    batman

---
+++ playwright.config.ts
@@ -0,0 +1,72 @@
+import {
+  defineConfig,
+  devices,
+  ScreenshotMode,
+  TraceMode,
+  VideoMode,
+} from '@playwright/test';
+import 'dotenv/config';
+
+const validPolicies = {
+  screenshot: [
+    'off',
+    'on',
+    'only-on-failure',
+    'on-first-failure',
+  ] as const satisfies ScreenshotMode[],
+  trace: [
+    'off',
+    'on',
+    'retain-on-failure',
+    'on-first-retry',
+    'on-all-retries',
+    'retain-on-first-failure',
+  ] as const satisfies TraceMode[],
+  video: ['off', 'on', 'retain-on-failure'] as const satisfies VideoMode[],
+} as const;
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: Boolean(process.env.CI),
+  retries: 1,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    baseURL: 'https://www.saucedemo.com',
+    screenshot: getValidPolicyMode('screenshot', 'PW_SCREENSHOT') || 'on',
+    trace: getValidPolicyMode('trace', 'PW_TRACE') || 'on-first-retry',
+    video: getValidPolicyMode('video', 'PW_VIDEO') || 'off',
+    testIdAttribute: 'data-test',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
+
+interface PolicyTypes {
+  screenshot: ScreenshotMode;
+  trace: TraceMode;
+  video: VideoMode;
+}
+
+function getValidPolicyMode<T extends keyof typeof validPolicies>(
+  policySubject: T,
+  envKey: string
+): PolicyTypes[T] | undefined {
+  const value = process.env[envKey];
+  if (!value) return;
+  if (validPolicies[policySubject].includes(value as any)) {
+    return value as PolicyTypes[T];
+  }
+  throw new Error(`"${value}" value is not valid for ${policySubject} policy!`);
+}
